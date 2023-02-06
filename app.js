67d1e7681ba0e7f74b743cdd0a5688bd8b179011
ლიკა გოგიშვილი <lgogishvili@geostat.gov.ge>
2023-02-06 10:42:49 +0400

    pushing updates

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const path = require("path");
 
-//routes
-const adminRoutes = require("./rotes/admin");
-const shopRoutes = require("./rotes/shop");
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//imported routes from other file
-app.use("/admin", adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  //   res.status(404).send("<h1>Page not found</h1>");
-  res.status(404).sendFile(path.join(__dirname, "./views/404.html"));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
-
-//using next
-// app.use("/", (req, res, next) => {
-//   console.log("This always runs");
-//   next();
-// });
